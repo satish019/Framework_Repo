ae50696ab9b3c94e5d14acbf7be47b33f66890e0
Satish <bokkasatish019@gmail.com>
2025-05-21 17:03:27 +0530

    modified those files

---
+++ SeleniumProject/src/test/java/Pack2/Organisation.java
@@ -0,0 +1,16 @@
+package Pack2;
+
+import org.testng.annotations.Test;
+
+public class Organisation {
+@Test
+	public void OrganisationTest() {
+	System.out.println("execute CreateOrganisation Test");
+	
+}
+@Test
+public void VerifyOrganisationTest() {
+System.out.println("execute ModifyOrganisation Test");
+}
+	
+}
