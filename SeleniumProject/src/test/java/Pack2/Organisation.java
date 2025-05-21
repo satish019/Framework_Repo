b6f0a11be3b6feba9b330af8b804cee44b43e894
Satish <bokkasatish019@gmail.com>
2025-05-21 18:11:58 +0530

    added test

--- SeleniumProject/src/test/java/Pack2/Organisation.java
+++ SeleniumProject/src/test/java/Pack2/Organisation.java
@@ -5,6 +5,15 @@ import org.testng.annotations.Test;
 public class Organisation {
 @Test
 	public void OrganisationTest() {
+	String URL = System.getProperty("url");
+	String BROWSER = System.getProperty("browser", "chrome");
+	String USERNAME = System.getProperty("username");
+	String PASSWORD = System.getProperty("password");
+	System.out.println( URL );
+	System.out.println(BROWSER);
+	System.out.println(USERNAME);
+	System.out.println(PASSWORD);
+	
 	System.out.println("execute CreateOrganisation Test");
 	
 }
